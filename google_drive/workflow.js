176497c6be8da39b4d96c3498c40c412da7cc675
Louis Cibot <louis@zenaton.com>
2019-10-10 11:15:39 +0200

    Add google_drive example

---
+++ google_drive/workflow.js
@@ -0,0 +1,34 @@
+const { workflow } = require("zenaton");
+
+module.exports = workflow("GoogleDriveExample", {
+  *handle() {
+    const googleDrive = this.connector("google_drive", "YOUR-CONNECTOR-ID");
+
+    // Get 20 files from the drive ordered by name
+    yield googleDrive.get("/files", {
+      query: { pageSize: 20, orderBy: "name" }
+    });
+
+    // Create a sample file in the drive
+    const response = yield googleDrive.post("/files", {
+      query: { uploadType: "simple" },
+      body: {
+        name: "My first created file with Zenaton"
+      }
+    });
+
+    const fileId = response.data.id;
+
+    // Update the name of the file and star it
+    yield googleDrive.patch(`/files/${fileId}`, {
+      query: { uploadType: "simple" },
+      body: {
+        name: "My first created file with Zenaton and starred",
+        starred: true
+      }
+    });
+
+    // Delete the example created file
+    yield googleDrive.delete(`/files/${fileId}`);
+  }
+});
