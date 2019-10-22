2069ff15f80c452a0e649477b2b5977fbe07009a
Gilles Barbier <gilles@zenaton.com>
2019-10-22 10:21:23 +0200

    Add SalesForce example

---
+++ salesforce/workflow.js
@@ -0,0 +1,11 @@
+const { workflow } = require("zenaton");
+
+workflow("SalesForceExample", {
+  *handle() {
+    const sf = this.connector('salesforce', 'your_connector_id');
+
+    yield sf.get('/data/v46.0', {headers: { salesforce_instance: "your_instance"}});
+    // https://www.asagarwal.com/how-to-determine-your-salesforce-instance-and-its-location/
+    
+  }
+});
