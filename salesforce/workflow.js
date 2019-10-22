3e2d385ad739287348eb2359b391a46f63c13165
Gilles Barbier <gilles@zenaton.com>
2019-10-22 10:22:51 +0200

    Update workflow.js

--- salesforce/workflow.js
+++ salesforce/workflow.js
@@ -4,7 +4,7 @@ workflow("SalesForceExample", {
   *handle() {
     const sf = this.connector('salesforce', "your_connector_id");
 
-    yield sf.get('/data/v46.0', {headers: { salesforce_instance: "your_instance"}});
+    yield sf.get("/data/v46.0", {headers: { salesforce_instance: "your_instance"}});
     // https://www.asagarwal.com/how-to-determine-your-salesforce-instance-and-its-location/
     
   }
