7e3a380af3766a4a0b6c5eb871901d23906525f6
Gilles Barbier <gilles@zenaton.com>
2019-10-22 10:47:23 +0200

    Update workflow.js

--- salesforce/workflow.js
+++ salesforce/workflow.js
@@ -5,7 +5,7 @@ workflow("SalesForceExample", {
     const sf = this.connector('salesforce', "your_connector_id");
 
     yield sf.get("/data/v46.0", {headers: { salesforce_instance: "your_instance"}});
-    // https://www.asagarwal.com/how-to-determine-your-salesforce-instance-and-its-location/
+    // example "eu29" - cf. https://www.asagarwal.com/how-to-determine-your-salesforce-instance-and-its-location/
     
   }
 });
