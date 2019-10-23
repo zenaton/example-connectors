67cc7b151bce26fee4a417cdd223253f8f0a87bd
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 14:28:57 +0200

    New syntax

--- cronofy/workflow.js
+++ cronofy/workflow.js
@@ -1,25 +1,21 @@
-const { workflow } = require("zenaton")
+module.exports.handle = function*() {
+  const cronofy = this.connector("cronofy", "YOUR-CONNECTOR-ID");
 
-module.exports = workflow("CronofyExample", {
-  *handle() {
-    const cronofy = this.connector("cronofy", "YOUR-CONNECTOR-ID");
+  // List calendars
+  const calendars = (yield cronofy.get("/calendars")).data.calendars
 
-    // List calendars
-    const calendars = (yield cronofy.get("/calendars")).data.calendars
-    
-    const first_calendar = calendars[0]
+  const first_calendar = calendars[0]
 
-    // Create an event
-    const payload = {
-      event_id: "qTtZdczOccgaPncGJaCiLg",
-      summary: "Board meeting",
-      description: "Discuss plans for the next quarter.",
-      start: "2020-08-05T15:30:00Z",
-      end: "2020-08-05T17:00:00Z",
-      location: {
-        "description": "Board room"
-      }
+  // Create an event
+  const payload = {
+    event_id: "qTtZdczOccgaPncGJaCiLg",
+    summary: "Board meeting",
+    description: "Discuss plans for the next quarter.",
+    start: "2020-08-05T15:30:00Z",
+    end: "2020-08-05T17:00:00Z",
+    location: {
+      "description": "Board room"
     }
-    const new_event = (yield cronofy.post(`calendars/${first_calendar.calendar_id}/events`, {body: payload})).data
   }
-})
+  const new_event = (yield cronofy.post(`calendars/${first_calendar.calendar_id}/events`, { body: payload })).data
+}
