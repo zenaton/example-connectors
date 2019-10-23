ff184ca1b3ed8b001870406352f22e5a83df58c0
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 15:02:25 +0200

    New syntax for Github example

--- github/workflow.js
+++ github/workflow.js
@@ -1,41 +1,37 @@
-const { workflow } = require("zenaton");
+module.exports = function* () {
+  const github = this.connector("github", "YOUR-CONNECTOR-ID");
 
-module.exports = workflow("GithubExample", {
-  *handle() {
-    const github = this.connector("github", "YOUR-CONNECTOR-ID");
+  // List my public repositories
+  yield github.get("/user/repos", { query: { visibility: "public" } });
 
-    // List my public repositories
-    yield github.get("/user/repos", { query: { visibility: "public" } });
-
-    // Create a new gist
-    const response = yield github.post("/gists", {
-      body: {
-        description: "Hello World Zenaton",
-        public: true,
-        files: {
-          "hello_world.js": {
-            content:
-              "function hello(name) { \n  console.log('Hello world ' + name) \n }"
-          }
+  // Create a new gist
+  const response = yield github.post("/gists", {
+    body: {
+      description: "Hello World Zenaton",
+      public: true,
+      files: {
+        "hello_world.js": {
+          content:
+            "function hello(name) { \n  console.log('Hello world ' + name) \n }"
         }
       }
-    });
+    }
+  });
 
-    const gistId = response.data.id;
+  const gistId = response.data.id;
 
-    // Update created gist to change description
-    yield github.patch(`/gists/${gistId}`, {
-      body: {
-        description: "Hello World from Zenaton & Bearer.sh"
-      }
-    });
+  // Update created gist to change description
+  yield github.patch(`/gists/${gistId}`, {
+    body: {
+      description: "Hello World from Zenaton & Bearer.sh"
+    }
+  });
 
-    // Add a star to this gist
-    yield github.put(`/gists/${gistId}/star`, {
-      headers: { "Content-Length": 0 }
-    });
+  // Add a star to this gist
+  yield github.put(`/gists/${gistId}/star`, {
+    headers: { "Content-Length": 0 }
+  });
 
-    // Finally delete the gist
-    yield github.delete(`/gists/${gistId}`);
-  }
-});
+  // Finally delete the gist
+  yield github.delete(`/gists/${gistId}`);
+}
