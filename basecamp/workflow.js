7bdbb3e795d140c5182f002c2e9e9571882dd7e5
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 14:18:29 +0200

    New syntax

--- basecamp/workflow.js
+++ basecamp/workflow.js
@@ -1,21 +1,17 @@
-const { workflow } = require("zenaton")
+module.exports.handle = function*(project_name, project_description) {
+  const basecamp = this.connector("basecamp", "YOUR-CONNECTOR-ID")
 
-module.exports = workflow("BasecampExample", {
-  *handle() {
-    const basecamp = this.connector("basecamp", "YOUR-CONNECTOR-ID")
+  const headers = { basecamp_account_id: "YOUR-BASECAMP-ACCOUNT-ID" }
 
-    const headers = { basecamp_account_id: "YOUR-BASECAMP-ACCOUNT-ID"}
+  // List projects
+  yield basecamp.get('/projects.json', { headers })
 
-    // List projects
-    yield basecamp.get('/projects.json', { headers })
+  // Create a project
+  const first_project = (yield basecamp.post('/projects.json', { headers, body: { name: project_name } })).data
 
-    // Create a project
-    const first_project = (yield basecamp.post('/projects.json', { headers, body: {name: 'First project'}})).data
+  // Update a project
+  yield basecamp.put(`/projects/${first_project.id}.json`, { headers, body: { description: project_description } })
 
-    // Update a project
-    yield basecamp.put(`/projects/${first_project.id}.json`, { headers, body: {description: 'Add some description'}})
-
-    // Delete project
-    yield basecamp.delete(`/projects/${first_project.id}.json`, { headers })
-  }
-})
+  // Delete project
+  yield basecamp.delete(`/projects/${first_project.id}.json`, { headers })
+}
