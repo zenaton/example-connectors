a71d20da975a11e67fbfa26a53c5117df5095566
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 14:59:04 +0200

    New syntax for Asana example

--- asana/workflow.js
+++ asana/workflow.js
@@ -1,26 +1,22 @@
-const { workflow } = require("zenaton")
+module.exports = function* (task_name) {
+  const asana = this.connector("asana", "YOUR-CONNECTOR-ID");
 
-module.exports = workflow("AsanaExample", {
-  *handle() {
-    const asana = this.connector("asana", "b975f4f0-f4c9-11e9-9259-a14e85f74896")
+  // Get info about me
+  const me = yield asana.get("/users/me")
 
-    // Get info about me
-    const me = yield asana.get("/users/me")
+  // List workspaces
+  const workspaces = (yield asana.get("/workspaces")).data.data
 
-    // List workspaces
-    const workspaces = (yield asana.get("/workspaces")).data.data
+  const first_workspace = workspaces[0]
 
-    const first_workspace = workspaces[0]
-
-    // Create a task
-    const payload = {
-      body: {
-        data: {
-          workspace: first_workspace.gid,
-          name: 'First task from Zenaton !',
-        }
+  // Create a task
+  const payload = {
+    body: {
+      data: {
+        workspace: first_workspace.gid,
+        name: task_name,
       }
     }
-    const new_task = (yield asana.post("/tasks", payload)).data.data
   }
-})
+  const new_task = (yield asana.post("/tasks", payload)).data.data
+}
