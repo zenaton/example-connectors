0b4c4e3c90ec0817da9955eee218f4890cbe400e
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 14:38:17 +0200

    New syntax

--- intercom/workflow.js
+++ intercom/workflow.js
@@ -1,20 +1,15 @@
-const { workflow } = require("zenaton")
+module.exports = function*(id, email, name) {
+  const intercom = this.connector("intercom", "YOUR-CONNECTOR-ID")
 
-module.exports = workflow("IntercomExample", {
-  *handle() {
-    const intercom = this.connector("intercom", "YOUR-CONNECTOR-ID");
+  // List users sorted by created_at DESC
+  const params = { query: { sort: 'created_at', order: 'desc' } }
+  const users = (yield intercom.get('users', params)).data.users
 
-    // List users sorted by created_at DESC
-    const params = {query: {sort: 'created_at', order: 'desc'}}
-    const users = (yield intercom.get('users', params)).data.users
-
-    // Create a user
-    const payload = {
-      user_id: '125',
-      email: 'wash2@serenity.io',
-      name: 'Hoban Washburne',
-      phone: '555671243'    
-    }
-    const new_user = yield intercom.post('users', {body: payload})
+  // Create a user
+  const payload = {
+    user_id: id,
+    email: email,
+    name: name
   }
-})
+  const new_user = yield intercom.post('users', { body: payload })
+}
