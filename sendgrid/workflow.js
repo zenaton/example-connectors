e6aca884764fc4fb0b4db1cf7b20edb96689bb1b
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 16:31:08 +0200

    Add Sendgrid workflow example

---
+++ sendgrid/workflow.js
@@ -0,0 +1,18 @@
+module.exports.handle = function* (from,to,subject,content) {
+  const sendgrid = this.connector("sendgrid", "YOUR-CONNECTOR-ID");
+
+  // Send text email
+  const payload = {
+    body: {
+      personalizations: [
+        {
+          to: [{ email: to }],
+        }
+      ],
+      content: [{ type: 'text/plain', value: content }],
+      subject: subject,
+      from: { email: from },
+    }
+  }
+  sendgrid.post("/mail/send", payload)
+}
