c10c41f16e65da19378ae291344cfd7edf6979db
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 15:07:17 +0200

    New syntax

--- unsplash/workflow.js
+++ unsplash/workflow.js
@@ -1,10 +1,6 @@
-const { workflow } = require("zenaton")
+module.exports = function* () {
+  const unsplash = this.connector("unsplash", "YOUR-CONNECTOR-ID")
 
-module.exports = workflow("UnsplashExample", {
-  *handle() {
-    const unsplash = this.connector("unsplash", "YOUR-CONNECTOR-ID")
-
-    // Search images
-    yield unsplash.get('/search/photos', { query: { query: "cat" } })
-  }
-})
+  // Search images
+  yield unsplash.get('/search/photos', { query: { query: "cat" } })
+}
