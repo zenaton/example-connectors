3d09911df70b2bf75ebff47bf49cc96fb2b4b12c
Antoine REYT <antoine.reyt@gmail.com>
2019-10-23 15:05:32 +0200

    Update syntax

--- google_calendar/workflow.js
+++ google_calendar/workflow.js
@@ -1,55 +1,51 @@
-const { workflow } = require("zenaton");
+module.exports = function*() {
+  const googleCalendar = this.connector(
+    "google_calendar",
+    "YOUR-CONNECTOR-ID"
+  )
 
-module.exports = workflow("GoogleCalendarExample", {
-  *handle() {
-    const googleCalendar = this.connector(
-      "google_calendar",
-      "YOUR-CONNECTOR-ID"
-    );
+  // Returns the calendars on my calendar list
+  const response = yield googleCalendar.get("/users/me/calendarList")
 
-    // Returns the calendars on my calendar list
-    const response = yield googleCalendar.get("/users/me/calendarList");
+  // Currently logged in calendar
+  const primaryCalendar = response.data.items.find(item => item.primary)
 
-    // Currently logged in calendar
-    const primaryCalendar = response.data.items.find(item => item.primary);
-
-    // Creates an event in my primary calendar
-    const event = yield googleCalendar.post(
-      `/calendars/${primaryCalendar.id}/events`,
-      {
-        body: {
-          summary: "Zenaton google calendar connector demo",
-          location: "800 Howard St., San Francisco, CA 94103",
-          description: "A chance to hear more about Zenaton",
-          start: {
-            dateTime: "2019-10-29T09:00:00-07:00",
-            timeZone: "America/Los_Angeles"
-          },
-          end: {
-            dateTime: "2019-10-29T17:00:00-07:00",
-            timeZone: "America/Los_Angeles"
-          }
+  // Creates an event in my primary calendar
+  const event = yield googleCalendar.post(
+    `/calendars/${primaryCalendar.id}/events`,
+    {
+      body: {
+        summary: "Zenaton google calendar connector demo",
+        location: "800 Howard St., San Francisco, CA 94103",
+        description: "A chance to hear more about Zenaton",
+        start: {
+          dateTime: "2019-10-29T09:00:00-07:00",
+          timeZone: "America/Los_Angeles"
+        },
+        end: {
+          dateTime: "2019-10-29T17:00:00-07:00",
+          timeZone: "America/Los_Angeles"
         }
       }
-    );
+    }
+  )
 
-    // Update the event
-    yield googleCalendar.patch(
-      `calendars/${primaryCalendar.id}/events/${event.data.id}`,
-      {
-        body: {
-          attendees: [
-            { email: "lpage@example.com" },
-            { email: "sbrin@example.com" }
-          ]
-        }
+  // Update the event
+  yield googleCalendar.patch(
+    `calendars/${primaryCalendar.id}/events/${event.data.id}`,
+    {
+      body: {
+        attendees: [
+          { email: "lpage@example.com" },
+          { email: "sbrin@example.com" }
+        ]
       }
-    );
+    }
+  )
 
-    // Delete the event and send notifications to all the attendees
-    yield googleCalendar.delete(
-      `calendars/${primaryCalendar.id}/events/${event.data.id}`,
-      { query: { sendUpdates: "all" } }
-    );
-  }
-});
+  // Delete the event and send notifications to all the attendees
+  yield googleCalendar.delete(
+    `calendars/${primaryCalendar.id}/events/${event.data.id}`,
+    { query: { sendUpdates: "all" } }
+  )
+}
