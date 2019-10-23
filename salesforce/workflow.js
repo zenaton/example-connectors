e2cbf3a05aba4ec319fac372e855ad5b789b34d2
Gilles Barbier <gilles@zenaton.com>
2019-10-23 14:09:48 +0200

    Update workflow.js

--- salesforce/workflow.js
+++ salesforce/workflow.js
@@ -1,11 +1,6 @@
-const { workflow } = require("zenaton");
+module.export.handle = function*() {
+  const sf = this.connector('salesforce', "your_connector_id");
 
-workflow("SalesForceExample", {
-  *handle() {
-    const sf = this.connector('salesforce', "your_connector_id");
-
-    yield sf.get("/data/v46.0", {headers: { salesforce_instance: "your_instance"}});
-    // example "eu29" - cf. https://www.asagarwal.com/how-to-determine-your-salesforce-instance-and-its-location/
-    
-  }
-});
+  yield sf.get("/data/v46.0", {headers: { salesforce_instance: "your_instance"}});
+  // example "eu29" - cf. https://www.asagarwal.com/how-to-determine-your-salesforce-instance-and-its-location
+};
