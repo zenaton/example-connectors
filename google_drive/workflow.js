0948e79550170b44e8987f6b92f210e8b2a9bbed
Gilles Barbier <gilles@zenaton.com>
2019-10-23 14:12:58 +0200

    Update workflow.js

--- google_drive/workflow.js
+++ google_drive/workflow.js
@@ -1,34 +1,30 @@
-const { workflow } = require("zenaton");
+module.export.handle = function*() {
+  const googleDrive = this.connector("google_drive", "YOUR-CONNECTOR-ID");
 
-module.exports = workflow("GoogleDriveExample", {
-  *handle() {
-    const googleDrive = this.connector("google_drive", "YOUR-CONNECTOR-ID");
+  // Get 20 files from the drive ordered by name
+  yield googleDrive.get("/files", {
+    query: { pageSize: 20, orderBy: "name" }
+  });
 
-    // Get 20 files from the drive ordered by name
-    yield googleDrive.get("/files", {
-      query: { pageSize: 20, orderBy: "name" }
-    });
+  // Create a sample file in the drive
+  const response = yield googleDrive.post("/files", {
+    query: { uploadType: "simple" },
+    body: {
+      name: "My first created file with Zenaton"
+    }
+  });
+  
+  const fileId = response.data.id;
 
-    // Create a sample file in the drive
-    const response = yield googleDrive.post("/files", {
-      query: { uploadType: "simple" },
-      body: {
-        name: "My first created file with Zenaton"
-      }
-    });
+  // Update the name of the file and star it
+  yield googleDrive.patch(`/files/${fileId}`, {
+    query: { uploadType: "simple" },
+    body: {
+      name: "My first created file with Zenaton and starred",
+      starred: true
+    }
+  });
 
-    const fileId = response.data.id;
-
-    // Update the name of the file and star it
-    yield googleDrive.patch(`/files/${fileId}`, {
-      query: { uploadType: "simple" },
-      body: {
-        name: "My first created file with Zenaton and starred",
-        starred: true
-      }
-    });
-
-    // Delete the example created file
-    yield googleDrive.delete(`/files/${fileId}`);
-  }
-});
+  // Delete the example created file
+  yield googleDrive.delete(`/files/${fileId}`);
+};
